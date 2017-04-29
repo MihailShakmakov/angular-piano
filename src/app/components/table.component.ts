d9b0daa0d7ad886457684d8a999e99343aabcbca
Michael Shakmakov <Michael_Shakmakov@epam.com>
2017-04-30 00:21:52 +0400

    preserve query params removing

--- src/app/components/table.component.ts
+++ src/app/components/table.component.ts
@@ -18,16 +18,16 @@ import IColumns = entities.table.IColumns;
             <tbody>
             <tr *ngFor="let result of items" class="table-row">
                 <td *ngIf="columns.author.show" class="table-row__cell">
-                    <a *ngIf="columns.author.clickable" [routerLink]="['author/'+result.owner.user_id]" preserveQueryParams="true">{{result.owner.display_name}}</a>
+                    <a *ngIf="columns.author.clickable" [routerLink]="['author/'+result.owner.user_id]" queryParamsHandling="merge">{{result.owner.display_name}}</a>
                     <span *ngIf="!columns.author.clickable">{{result.owner.display_name}}</span>
                 </td>
                 <td *ngIf="columns.theme.show" class="table-row__cell">
-                    <a *ngIf="columns.theme.clickable" [routerLink]="[author/result.owner.user_id]" preserveQueryParams="true">{{result.title + '(' + result.answer_count + ')'}}</a>
+                    <a *ngIf="columns.theme.clickable" [routerLink]="[author/result.owner.user_id]" queryParamsHandling="merge">{{result.title + '(' + result.answer_count + ')'}}</a>
                     <span *ngIf="!columns.theme.clickable">{{result.title + '(' + result.answer_count + ')'}}</span>
                 </td>
                 <td *ngIf="columns.tags.show" class="table-row__cell">
                     <span *ngFor="let tag of result.tags; let i = index">
-                        <a *ngIf="columns.tags.clickable" [routerLink]="['tag/'+tag]" preserveQueryParams="true">{{tag}}</a>
+                        <a *ngIf="columns.tags.clickable" [routerLink]="['tag/'+tag]" queryParamsHandling="merge">{{tag}}</a>
                         <span *ngIf="!columns.tags.clickable">{{tag}}<span *ngIf="i !== (result.tags.length - 1)">, </span></span>
                     </span>
                 </td>
